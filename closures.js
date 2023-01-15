85292fd4e27e8a7dfea573952dafdbc67d849ddf
rudeseagull <jun.capellen@gmail.com>
2023-01-15 14:38:34 +0100

    Challenges 5 to 7

--- closures.js
+++ closures.js
@@ -103,3 +103,69 @@ const onceFunc = once(addByTwo);
 console.log(onceFunc(4));  // => should log 6
 console.log(onceFunc(10));  // => should log 6
 console.log(onceFunc(9001));  // => should log 6
+
+// Challenge 5
+// Write a function after that takes the number of times the callback needs to
+// be called before being executed as the first parameter and the callback as 
+// the second parameter.
+
+function after(count, func) {
+    let counter = 1;
+    function doWhenCountIsReached() {
+        if (counter >= count) {
+            return func();
+        } else {
+            counter++;
+        }
+    }
+    return doWhenCountIsReached;
+}
+
+const called = function () {
+    console.log('hello')
+};
+const afterCalled = after(3, called);
+afterCalled(); // => nothing is printed
+afterCalled(); // => nothing is printed
+afterCalled(); // => 'hello' is printed
+afterCalled(); // => 'hello' is printed
+
+// Challenge 6
+// Write a function delay that accepts a callback as the first parameter 
+// and the wait in milliseconds before allowing the callback to be invoked as
+// the second parameter. Any additional arguments after wait are provided 
+// to func when it is invoked.
+
+function delay(func, wait) {
+    function delayFunc(...args) {
+        setTimeout(() => func(args), wait)
+    }
+    return delayFunc;
+}
+
+const delayFunc = delay((input) => console.log(input), 3000);
+delayFunc("hello");
+
+// Challenge 7
+// Write a function rollCall that accepts an array of names and returns a function. 
+// The first time the returned function is invoked, it should log the first name 
+// to the console. The second time it is invoked, it should log the second name 
+// to the console, and so on, until all names have been called. 
+// Once all names have been called, it should log 'Everyone accounted for'.
+
+function rollCall(names) {
+    let counter = 0;
+    function log() {
+        counter < names.length
+            ? console.log(names[counter])
+            : console.log("Everyone accounted for")
+        counter++;
+    }
+    return log;
+}
+
+const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
+rollCaller() // => should log 'Victoria'
+rollCaller() // => should log 'Juan'
+rollCaller() // => should log 'Ruth'
+rollCaller() // => should log 'Everyone accounted for'
